40d0ec6c4d1f270802e065bd28edc6904228715d
Alexis Abner <alexiproj@gmail.com>
2024-10-14 14:50:34 -0600

    create SectionWrapper.jsx and utils folder with data.js

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -5,8 +5,8 @@ function SectionWrapper(props) {
   return (
    <section className='min-h-screen flex flex-col gap-10'>
     <div className='bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center'>
-        <p>{header}</p>
-        <h2>{title[0]} {title[1]} <span>{title[2]}</span>{title[3]}</h2>
+        <p className='uppercase font-medium'>{header}</p>
+        <h2 className='font-semi-bold text-3xl sm:text-4xl md-text-5xl lg-6xl'>{title[0]} {title[1]} <span className='text-blue-400'>{title[2]}</span>{title[3]}</h2>
     </div>
    </section>
   )
